417ece1738974b6f58b6361fb6c69d68ef3acc24
Filipe Guimarães <fmtfg99@gmail.com>
2021-04-22 22:57:16 +0100

    fix menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
-        System.out.println("(Type m to access the menu)");
         Network p2p = Network.getInstance();
 
         if(args.length==1){
             p2p.loadPeersFromConfig(args[0]);
         }
 
-        Scanner sc = new Scanner(System.in);
-        if (sc.nextLine().equals("m")) {
-            Menu.menu();
-        } else {
-            System.out.println("(Type m to access the menu)");
-        }
+        Menu.menu();
 
     }
 
--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -4,34 +4,35 @@ import java.util.Scanner;
 public class Menu {
 
     public static void menu() throws IOException {
-        System.out.println("####################################");
-        System.out.println("# 1: Show connected peers.         #");
-        System.out.println("# 2: Show disconnected peers.      #");
-        System.out.println("# 3: Search a file in the network. #");
-        System.out.println("####################################");
-
-        System.out.println("Option:");
-
-        Scanner sc = new Scanner(System.in);
-        switch (sc.nextInt()) {
-            case 1:
-                Network.getInstance().printConnectedPeers();
-                break;
-            case 2:
-                Network.getInstance().printDisconnectedPeers();
-                break;
-            case 3:
-                break;
-            default:
-                System.out.println("Invalid option!");
-        }
-
+        System.out.println("(Type m to access the menu)");
         Scanner s = new Scanner(System.in);
-        if (s.nextLine().equals("m")) {
+        if (!s.nextLine().equals("m")) {
             Menu.menu();
         } else {
-            System.out.println("(Type m to access the menu)");
+            System.out.println("####################################");
+            System.out.println("# 1: Show connected peers.         #");
+            System.out.println("# 2: Show disconnected peers.      #");
+            System.out.println("# 3: Search a file in the network. #");
+            System.out.println("####################################");
+
+            System.out.println("Option:");
+
+            Scanner sc = new Scanner(System.in);
+            switch (sc.nextInt()) {
+                case 1:
+                    Network.getInstance().printConnectedPeers();
+                    break;
+                case 2:
+                    Network.getInstance().printDisconnectedPeers();
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
         }
+
+        menu();
     }
 
 
